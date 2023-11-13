9e1299149cd1a3bd41569bc2a844077b47817069
YamAlmog <yamalmog14@gmail.com>
2023-11-14 00:57:46 +0200

    add endpoint for products

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException 
 from models import Department, Product
 
 app = FastAPI()
 
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 departments = []
 
@@ -21,7 +22,7 @@ async def get_department(department_id: int):
     for department in departments:
         if department.id == department_id:
             return {"Department": department}
-    return {"message": f"Department with id:{department_id} not found"}
+    raise HTTPException(status_code=404, detail="department not found")
 
 
 # Create a department
@@ -35,9 +36,11 @@ async def create_department(department: Department):
 async def update_department(department_id: int, department_obj: Department):
     for department in departments:
         if department.id == department_id:
+            department.id = department_obj.id
             department.name = department_obj.name
             return {"Department": department}
-    return {"message": "No departments found to update"}
+        
+    raise HTTPException(status_code=404, detail="department not found")
 
 # Delete a department
 @app.delete("/departments/{department_id}")
@@ -46,5 +49,51 @@ async def delete_department(department_id: int):
         if department.id == department_id:
             departments.remove(department)
             return {"message": "Department has been DELETED!"}
-    return {"message": f"Department with id:{department_id} not found"}
+    return {"message": f"Department with id:{department_id} was not found"}
+
+
+products = []
+
+# Get all products
+@app.get("/products")
+async def get_products():
+    return {"Products": products}
+
+# Get single product
+@app.get("/products/{product_id}")
+async def get_product(product_id: int):
+    for product in products:
+        if product.id == product_id:
+            return {"Products": product}
+    return {"message": f"Product with id:{product_id} was not found"}
+
+# Create a product
+@app.post("/products")
+async def create_product(product: Product):
+    products.append(product)
+    return {"message": "Product has been added"}
+
+# Update a Product
+@app.put("/products/{product_id}")
+async def update_product(product_id: int, product_obj: Product):
+    for product in products:
+        if product.id == product_id:
+            product.id == product_obj.id
+            product.name = product_obj.name
+            product.price = product_obj.price
+            product.quantity = product_obj.quantity
+            product.department_id = product_obj.department_id
+            product.specifications = product_obj.specifications
+            return {"Product": product}
+    return {"message": "No Products found to update"}
+
+# Delete a product
+@app.delete("/products/{product_id}")
+async def delete_product(product_id: int):
+    for product in products:
+        if product.id == product_id:
+            products.remove(product)
+            return {"message": "Product has been DELETED!"}
+    return {"message": f"Product with id:{product_id} was not found"}
+
 
