63d786497d93007ebae819c3271298766968af5b
YamAlmog <yamalmog14@gmail.com>
2023-11-15 18:52:17 +0200

    trying to create test function for product IP

--- test_main.py
+++ test_main.py
@@ -3,13 +3,12 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_if_root_is_correct():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-def test_get_department():
+def test_get_departments():
     response = client.get('/departments')
     assert response.status_code == 200
     assert "Departments" in response.json()
@@ -35,7 +34,58 @@ def test_update_department():
 
 
 def test_delete_department():
-    response = client.delete('/departments/0')
+    response = client.delete("/departments/0")
     assert response.status_code == 200
     assert response.json() == {"message": f"Department id: {0} has been DELETED!"}
 
+'''def test_get_specific_department():
+    response = client.get(f"/departments/{0}")
+    assert response.status_code == 200
+    assert response.json()["Department"]["Key"] == 0
+    assert response.json()["Department"]["Value"] == "dairy"'''
+
+
+def test_create_department():
+    new_dep = {"name": "dairy"}
+    response = client.post('/departments', json=new_dep)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Department has been added"}
+
+def test_get_products():
+    response = client.get('/products')
+    assert response.status_code == 200
+    assert "Products" in response.json()
+
+
+def test_create_product():
+    new_prod = {
+                "name": "Cheese",
+                "price": 10.9,
+                "quantity": 12,
+                "department_id": 1,
+                "specifications": "fat 25%"
+                }
+    response = client.post('/products', json=new_prod)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Product has been added"}
+
+def test_get_specific_product():
+    response = client.get('/products/0')
+    assert response.status_code == 200
+    expected_response = {
+                            "Products": {
+                                "Key": 1,
+                                "Value": ["Cheese", 10.9, 12, 1, "fat 25%"]
+                            }
+                        }
+   
+    assert response.json() == expected_response
+    #assert response.json()["Products"]["Key"] == 0
+        
+        
+
+def test_update_product():
+    pass 
+
+def test_delete_product():
+    pass 
